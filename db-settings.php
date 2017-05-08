121737acb4f66cd0533e713258e89e24cd12fb34
prasad26 <prasadbpansare@gmail.com>
2017-05-08 16:38:27 -0400

    Update db-settings.php

--- db-settings.php
+++ db-settings.php
@@ -1,36 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: prasa
- * Date: 3/20/2017
- * Time: 12:39 PM
- */
-$db_host = "sql9.freemysqlhosting.net"; //Host address (most likely localhost)
-$db_name = "sql9173381"; //Name of Database
-$db_user = "sql9173381"; //Name of database user
-$db_pass = "R99eCtjt8h"; //Password for database user
-$db_table_prefix = ""; // if the table prefix exists use this variable as a global
-
-
-//following variable declaration is for next class :)
-GLOBAL $errors;
-GLOBAL $successes;
-
-$errors = array();
-$successes = array();
-
-/* Create a new mysqli object with database connection parameters */
-
-$mysqli = new mysqli($db_host, $db_name, $db_user, $db_pass);
-GLOBAL $mysqli;
-
-if(mysqli_connect_errno()) {
-    //display the reason for mysql connection error.
-    echo "Connection Failed: " . mysqli_connect_errno();
-    exit();
-
-}else{
-    //echo "Connection Successful";
-}
-
-?>
+	        define("DB_SERVER", "sql9.freemysqlhosting.net");
+	        define("DB_USER", "sql9173381");
+	        define("DB_PASS", "R99eCtjt8h");
+	        define("DB_NAME", "sql9173381");
+	
+	  // 1. Create a database connection
+	  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+	  // Test if connection succeeded
+	  if(mysqli_connect_errno()) {
+	    die("Database connection failed: " . 
+	         mysqli_connect_error() . 
+	         " (" . mysqli_connect_errno() . ")"
+	    );
+	  } 
+	  
+	 
+	
+	?>
